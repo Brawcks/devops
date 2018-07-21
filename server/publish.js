1a364b84c0c6cc4ac34d4ea729d037b0f29eb500
Vincent COFFIN <coffin.vincent.sp@gmail.com>
2018-07-22 01:55:14 +0200

    IMP: routes when logged in or not

--- server/publish.js
+++ server/publish.js
@@ -5,3 +5,8 @@ const self = this.Meteor;
 Meteor.publish('recipes', function() {
     return Recipes.find({author: self.userId()})
 })
+
+Meteor.publish('singleRecipe', function(id) {
+    check(id, String)
+    return Recipes.find({_id: id})
+})
